f26672073b1a43a5dd93eb7f17eae0508b4ee5d9
Jako9 <cubecrafter.lpt@gmail.com>
2020-11-27 00:17:48 +0100

    Fixing listening only on first push 3/2

--- announcer.js
+++ announcer.js
@@ -88,7 +88,7 @@ client.on('ready', () =>{
 
 
   //Setup Status
-  client.user.setActivity("Flo ist ne geile Socke", {
+  client.user.setActivity("Max und Flo sind geile Socken", {
     type: "STREAMING",
     url: "https://www.twitch.tv/jako9"
   });
