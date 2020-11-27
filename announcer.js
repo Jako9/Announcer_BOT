611798d7839724b545f980351a1b00a26a261350
Maximilian Krebs <32494292+printerboi@users.noreply.github.com>
2020-11-27 20:34:31 +0100

    Update announcer.js

--- announcer.js
+++ announcer.js
@@ -87,7 +87,7 @@ client.on('ready', () =>{
   reactionMessage = channelReact.messages.cache.find(foo => true);
 
   //Setup Status
-  client.user.setActivity("I pulled this request...", {
+  client.user.setActivity("I pulled this request!", {
     type: "STREAMING",
     url: "https://www.twitch.tv/jako9"
   });
