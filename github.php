27120e1fefc408afd1f552dbba98b653006925db
Printerboi <Maximilian-krebs@online.de>
2020-11-27 19:16:48 +0100

    Github.php hinzugefügt

---
+++ github.php
@@ -0,0 +1,29 @@
+<?php
+
+
+function run() {
+    global $rawInput;
+
+    $postBody = $_POST['payload'];
+    $payload = json_decode($postBody);
+
+    if ($payload->repository->url == 'https://github.com/Jako9/Announcer_BOT' && $payload->ref == 'refs/heads/master') {
+
+        shell_exec('cd /home/fkoehler/bot/');
+        shell_exec('git pull');
+        
+        return true;
+    }
+    
+}
+
+try {
+    if (!isset($_POST['payload'])) {
+        echo "Works fine.";
+    } else {
+        run();
+    }
+} catch ( Exception $e ) {
+    $msg = $e->getMessage();
+    mail($error_mail, $msg, ''.$e);
+}
