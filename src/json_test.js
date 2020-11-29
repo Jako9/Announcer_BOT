7be6433bc7be36ff9f0cd0ab27a26f794565e938
Jako9 <cubecrafter.lpt@gmail.com>
2020-11-29 03:49:34 +0100

    Manuell schon bestehende Server hinzugefügt, Testumgebung entfernt

--- src/json_test.js
+++
@@ -1,7 +0,0 @@
-const jsonParser = require("./includes/jsonParser");
-const serverManager = require("./includes/serverManager");
-
-
-serverManager.readInServers();
-serverManager.addServer('123');
-serverManager.removeServer('123');
