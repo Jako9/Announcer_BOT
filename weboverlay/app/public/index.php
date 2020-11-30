8a68cfa280d64f956cbb964bd43b6c178e31342e
max <maximilian-krebs@online.de>
2020-12-01 00:02:33 +0100

    schiesse

---
+++ weboverlay/app/public/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+
+ echo("lauf doch endlich bitte :(");
