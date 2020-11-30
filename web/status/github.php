df2026bdb5a71218ade2d53cc2019f7453f9d95f
Maximilian Krebs <32494292+printerboi@users.noreply.github.com>
2020-11-30 22:21:06 +0100

    Update github.php

--- web/status/github.php
+++ web/status/github.php
@@ -10,7 +10,8 @@ function run() {
     if ($payload->repository->url == 'https://github.com/Jako9/Announcer_BOT' && $payload->ref == 'refs/heads/master') {
 
         shell_exec("killall node");
-        shell_exec("cd .. && git pull && ./restart.sh");
+        shell_exec("git pull");
+        shell_exec("./restart.sh");
         
         return true;
     }
