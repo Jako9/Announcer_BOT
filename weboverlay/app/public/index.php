d2b2c783f803eb112e59feaef40845bc01608ed4
max <maximilian-krebs@online.de>
2020-12-04 23:04:45 +0100

    Favicon hinzugefügt

--- weboverlay/app/public/index.php
+++ weboverlay/app/public/index.php
@@ -361,6 +361,8 @@ function handleLogReset(){
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 
     <link rel="stylesheet" href="overlay.css">
+
+    <link rel="shortcut icon" type="image/ico" href="favicon.ico"/>
     </head>
     <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
