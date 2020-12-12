2eeb6513f612a00f797af4f82f0e0bc87791a727
max <maximilian-krebs@online.de>
2020-12-12 15:01:23 +0100

    absolute Pfade sind cool

--- weboverlay/app/public/index.php
+++ weboverlay/app/public/index.php
@@ -621,7 +621,7 @@ function getStatistics(){
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/js-cookie@rc/dist/js.cookie.min.js"></script>
 
-    <script type='text/javascript' src="./script.js"></script>
+    <script type='text/javascript' src="http://announcer.jmk.cloud/script.js"></script>
     </body>
     </html>
 
