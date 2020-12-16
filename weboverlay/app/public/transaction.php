4c6a81cf6c0c6be10c6e9b77a9a942d1aaba9a9e
max <maximilian-krebs@online.de>
2020-12-16 23:20:36 +0100

    Fehlerkorrektur falscher Objektname

--- weboverlay/app/public/transaction.php
+++ weboverlay/app/public/transaction.php
@@ -8,11 +8,11 @@ if(isset($_POST['transID']) && isset($_POST['state'])){
     $pendingPaymentsArray = $pendingPayments->transactions;
 
     if($pendingPaymentsArray != null){
-        foreach ($pendingPayments->transactions as $key => $transactionJson) {
+        foreach ($pendingPaymentsArray as $key => $transactionJson) {
             $transaction = json_decode($transactionJson);
     
-            if($transaction->transId != null){
-                if($transaction->transId == $transactionId){
+            if($transaction->transID != null){
+                if($transaction->transID == $transactionId){
                     $transaction->state = $state;
                 }
             }
