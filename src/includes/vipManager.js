96442fb9e8ad19619c4b2db94343ecaac98d975e
Jako9 <cubecrafter.lpt@gmail.com>
2020-12-16 22:13:40 +0100

    So

--- src/includes/vipManager.js
+++ src/includes/vipManager.js
@@ -6,17 +6,19 @@ const PATH = "/var/www/git.jmk.cloud/html/Announcer_BOT";
 module.exports = {
     becomeVIP: function(message){
       https.get('https://hook.integromat.com/rq89fjoouy985of9qg8tltpjgynnhj3a', (resp) => {
-        let data = '';
+  let data = '';
 
-        // A chunk of data has been recieved.
-        resp.on('data', (chunk) => {
-          data += chunk;
-      });
-      http.createServer(function(request, response) {
-        if( req.method === 'POST' ) {
-          res.writeHead(200, {'Content-Type': 'text/plain'});
-          res.end('Hello, World!\n');
-        }
-    }).listen(80);
-    }
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log(JSON.parse(data).explanation);
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
 }
