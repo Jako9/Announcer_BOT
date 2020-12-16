acacbd7e10a5efc5a8217d50f5297911e6bc43c1
max <maximilian-krebs@online.de>
2020-12-17 00:14:38 +0100

    Pending lol

--- src/includes/vipManager.js
+++ src/includes/vipManager.js
@@ -18,7 +18,7 @@ module.exports = {
         let jsonData = JSON.parse(data);
         let link = jsonData.paypalLink;
         if(link){
-          let transactions = jsonParser.read(PATH + "/config/pendingpayments.json").transactions;
+          let transactions = jsonParser.read(PATH + "/config/pendingPayments.json").transactions;
           let transID = jsonData.transID;
           let userID = message.author.id;
           transactions.add({
@@ -26,7 +26,7 @@ module.exports = {
             "userID" : userID,
             "status" : "Pending"
           });
-          jsonParser.write(PATH + "/config/pendingpayments.json", transactions);
+          jsonParser.write(PATH + "/config/pendingPayments.json", transactions);
           message.reply(link);
         }else{
           message.reply("Fehler bei der Transaktion, bitte versuche es erneut!");
