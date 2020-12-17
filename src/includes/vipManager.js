8112c3f96b0867af69eba8bc8f8271bdf0762922
Jako9 <cubecrafter.lpt@gmail.com>
2020-12-17 13:31:02 +0100

    Paypal pic

--- src/includes/vipManager.js
+++ src/includes/vipManager.js
@@ -2,72 +2,39 @@ const jsonParser = require('./jsonParser.js');
 var https = require("https");
 const PATH = "/var/www/git.jmk.cloud/html/Announcer_BOT";
 
-function buildEmbed(){
-  /*let embed = {
-	color: 0x0099ff,
-	title: 'Become a VIP',
-	url: 'https://www.paypal.com/cgi-bin/webscr?cmd=_express-checkout&token=EC-17V25142MM4028412',
-	author: {
-		name: 'Announcer_BOT',
-		icon_url: 'https://i.imgur.com/wSTFkRM.png',
-		url: 'http://announcer.jmk.cloud',
-	},
-	description: 'You want to become a VIP? \n Just follow the instructions and you will have a custom joinsound in no time!',
-	thumbnail: {
-		url: 'https://i.imgur.com/wSTFkRM.png',
-	},
-	fields: [
-		{
-			name: 'How do I become a VIP?',
-			value: 'Just click on the paypal link below and follow the instructions. After you are done, you can send a .wav file (max length 8 sec.) to the bot and your custom joinsound is ready!',
-		},
-    {
-      name: '\u200B',
-      value: '\u200B',
-    },
-    }
-	],
-	image: {
-		url: 'https://i.imgur.com/wSTFkRM.png',
-	},
-	timestamp: new Date(),
-	footer: {
-		text: 'Some footer text here',
-		icon_url: 'https://i.imgur.com/wSTFkRM.png',
-	},
-};*/
-let embed = {
-  color: 0x0099ff,
-  title: 'Become a VIP',
-  url: 'https://www.paypal.com/cgi-bin/webscr?cmd=_express-checkout&token=EC-17V25142MM4028412',
-  author: {
-		name: 'Announcer_BOT',
-		icon_url: 'https://i.imgur.com/wSTFkRM.png',
-		url: 'http://announcer.jmk.cloud'
-	},
-  description: 'You want to become a VIP? \n Just follow the instructions and you will have a custom joinsound in no time!',
-  thumbnail: {
-		url: 'https://i.imgur.com/wSTFkRM.png'
-	},
-  fields: [
-		{
-			name: 'How do I become a VIP?',
-			value: 'Just click on the paypal link below and follow the instructions. After you are done, you can send a .wav file (max length 8 sec.) to the bot and your custom joinsound is ready!'
-		},
-    {
-      name: '\u200B',
-      value: '\u200B'
-    }
-	],
-  image: {
-		url: 'https://www.paypalobjects.com/webstatic/icon/pp258.png'
-	},
-  timestamp: new Date(),
-  footer: {
-		text: 'Some footer text here',
-		icon_url: 'https://i.imgur.com/wSTFkRM.png'
-	}
-};
+function buildEmbed(link){
+  let embed = {
+    color: 0x0099ff,
+    title: 'Become a VIP',
+    url: link,
+    author: {
+  		name: 'Announcer_BOT',
+  		icon_url: 'https://i.imgur.com/wSTFkRM.png',
+  		url: 'http://announcer.jmk.cloud'
+  	},
+    description: 'You want to become a VIP? \n Just follow the instructions and you will have a custom joinsound in no time!',
+    thumbnail: {
+  		url: 'https://i.imgur.com/wSTFkRM.png'
+  	},
+    fields: [
+  		{
+  			name: 'How do I become a VIP?',
+  			value: 'Just click on the paypal link below and follow the instructions. After you are done, you can send a .wav file (max length 8 sec.) to the bot and your custom joinsound is ready!'
+  		},
+      {
+        name: '\u200B',
+        value: '\u200B'
+      }
+  	],
+    image: {
+  		url: 'https://www.paypalobjects.com/webstatic/icon/pp258.png'
+  	},
+    timestamp: new Date(),
+    footer: {
+  		text: 'Some footer text here',
+  		icon_url: 'https://i.imgur.com/wSTFkRM.png'
+  	}
+  };
   return embed;
 }
 
@@ -111,7 +78,7 @@ module.exports = {
   },
 
   becomeVIPTest: function(message){
-    let exampleEmbed = buildEmbed();
+    let exampleEmbed = buildEmbed("https://www.paypal.com/cgi-bin/webscr?cmd=_express-checkout&token=EC-17V25142MM4028412");
     message.author.send({ embed: exampleEmbed}).catch();
     if(message.guild) message.reply("Check your dms ;). If they are empty, your dms are probably closed. In this case open them and try again.");
     }
