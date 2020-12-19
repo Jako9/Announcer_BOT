465ef99ab1bf87df93dc97e46a342a6e0c52f45e
max <maximilian-krebs@online.de>
2020-12-19 20:20:38 +0100

    lenthg

--- src/includes/interactionManager.js
+++ src/includes/interactionManager.js
@@ -12,12 +12,12 @@ module.exports = {
   //Create a list of all commands and their usage
   help: function (message, prefix, instructions){
     let page = 1;
-    if(message.content.split(' ').lenght == 2 && !isNaN(message.content.split(' ')[1])){
+    if(message.content.split(' ').length == 2 && !isNaN(message.content.split(' ')[1])){
       page = message.content.split(' ')[1];
     }
     logManager.writeDebugLog("Seite = " + page);
     logManager.writeDebugLog("Ist eine Nummer = " + !isNaN(message.content.split(' ')[1]));
-    logManager.writeDebugLog("Länge gleich 2 = " + (message.content.split(' ').lenght == 2));
+    logManager.writeDebugLog("Länge gleich 2 = " + (message.content.split(' ').length == 2));
     var msg = '``` \n------------------------------------------------------------- \n' +
               'The bot should connect and disconnect automatically but if there are ' +
               'any problems \nor if you want to customize usage you can use the following commands' +
