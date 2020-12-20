1a12b6fa3bb4897bc8dc07b4d9c4b10dbdf63a25
max <maximilian-krebs@online.de>
2020-12-20 19:57:53 +0100

    ich push ma

--- src/includes/serverManager.js
+++ src/includes/serverManager.js
@@ -92,7 +92,7 @@ module.exports = {
 
         for(i=1; i <= instructions.length; i++){
             instructionsTmp.push({
-                "name": instructions[i-1],
+                "name": instructionsFirst[i-1][0],
                 "type": i
             });
         }
