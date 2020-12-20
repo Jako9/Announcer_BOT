c59c4dabd744e9a2a2de4856ff95bd0e8fa58009
max <maximilian-krebs@online.de>
2020-12-20 01:44:10 +0100

    Datenbankbefüllung

--- src/includes/databaseManager.js
+++ src/includes/databaseManager.js
@@ -20,9 +20,135 @@ module.exports = {
     addUser: function(userID, username, avatar, joinsound){
         connection.connect();
         
-        let q = "INSERT INTO users (userID, username, avatar, isVip, joinsound) VALUES ('"+ userID + "', '" + username + "', '" + avatar + "', '0', '"+ joinsound + "')";
+        try{
+            let q = "INSERT INTO users (userID, username, avatar, isVip, joinsound) VALUES ('"+ userID + "', '" + username + "', '" + avatar + "', '0', '"+ joinsound + "')";
+        }catch(err){
+            return false;
+        }
 
         connection.query(q);
+        connection.end();
+
+        return true;
+    },
+
+    getAllUsers: function(){
+        connection.connect();
+
+        let q = "SELECT * FROM users";
+
+        connection.query(q, (error, results) => {
+            if(error){
+                return false;
+            }
+
+            return results;
+        });
+        connection.end();
+    },
+
+    getUser: function(userID){
+        connection.connect();
+
+        let q = "SELECT * FROM users WHERE userID=" + userID;
+
+        connection.query(q, (error, results) => {
+            if(error){
+                return false;
+            }
+
+            return results;
+        });
+        connection.end();
+    },
+
+    getVip: function(userID){
+        connection.connect();
+
+        let q = "SELECT isVip FROM users WHERE userID=" + userID;
+
+        connection.query(q, (error, results) => {
+            if(error){
+                return false;
+            }
+
+            return results;
+        });
+        connection.end();
+    },
+
+    setVip: function(userID){
+        connection.connect();
+
+        let q = "UPDATE users SET isVIP=1 WHERE userID=" + userID;
+
+        connection.query(q, (error, results) => {
+            if(error){
+                return false;
+            }
+
+            return results;
+        });
+        connection.end();
+    },
+
+    getName: function(userID){
+        connection.connect();
+
+        let q = "SELECT username FROM users WHERE userID=" + userID;
+
+        connection.query(q, (error, results) => {
+            if(error){
+                return false;
+            }
+
+            return results;
+        });
+        connection.end();
+    },
+
+    setInformation: function(userID, username, avatar){
+        connection.connect();
+
+        let q = "UPDATE users SET username="+ username +", avatar="+ avatar +" WHERE userID=" + userID;
+
+        connection.query(q, (error, results) => {
+            if(error){
+                return false;
+            }
+
+            return results;
+        });
+        connection.end();
+    },
+
+    getJoinsound: function(userID){
+        connection.connect();
+
+        let q = "SELECT joinsound FROM users WHERE userID=" + userID;
+
+        connection.query(q, (error, results) => {
+            if(error){
+                return false;
+            }
+
+            return results;
+        });
+        connection.end();
+    },
+
+    setJoinsound: function(userID, joinsound){
+        connection.connect();
+
+        let q = "UPDATE users SET joinsound="+ joinsound +" WHERE userID=" + userID;
+
+        connection.query(q, (error, results) => {
+            if(error){
+                return false;
+            }
+
+            return results;
+        });
         connection.end();
     }
 };
