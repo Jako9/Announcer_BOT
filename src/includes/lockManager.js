5f6dea0d7990156e492393d41ac085f71094e463
Jako9 <cubecrafter.lpt@gmail.com>
2020-12-22 00:52:50 +0100

    dnd work

--- src/includes/lockManager.js
+++ src/includes/lockManager.js
@@ -6,9 +6,9 @@ function lockChannel (message){
   let channel = message.member.voice.channel;
   serverManager.setChannelSize(channel.guild.id, channel.userLimit);
   serverManager.setWhoLocked(channel.guild.id,message.member.id);
-  channel.setUserLimit(message.member.voice.channel.members.size).then(
+  channel.setUserLimit(message.member.voice.channel.members.size).then({
     channel.setName(name).catch();
-  );
+  });
 }
 
 //Schließt einen Raum ab
