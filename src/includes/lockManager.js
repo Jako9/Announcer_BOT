e9683f2a322a74d85414b5e086be8d6b16a58b23
Jako9 <cubecrafter.lpt@gmail.com>
2020-12-22 00:56:02 +0100

    dnd work

--- src/includes/lockManager.js
+++ src/includes/lockManager.js
@@ -6,6 +6,7 @@ function lockChannel (message){
   let channel = message.member.voice.channel;
   serverManager.setChannelSize(channel.guild.id, channel.userLimit);
   serverManager.setWhoLocked(channel.guild.id,message.member.id);
+  let name = "🔒 " + channel.name;
   channel.setUserLimit(message.member.voice.channel.members.size).then(limChannel =>{
     limChannel.setName(name).catch();
   });
