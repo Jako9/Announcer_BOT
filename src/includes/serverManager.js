e5a609097d4322831b2df6e215250c3f02c066c5
Jako9 <cubecrafter.lpt@gmail.com>
2020-12-23 04:09:43 +0100

    -Debug

--- src/includes/serverManager.js
+++ src/includes/serverManager.js
@@ -359,10 +359,7 @@ module.exports = {
             dbServer["whoLocked"] = null;
             dbServer["reactionMessage"] = null;
             dbServer["lockedChannel"] = null;
-            logManager.writeDebugLog("Server: " + dbServer.name);
-            logManager.writeDebugLog("JSON: " + dbServer.instructions);
             dbServer.instructions = JSON.parse(dbServer.instructions).instructions;
-            logManager.writeDebugLog("WORKED");
             dbServer.whitelist = JSON.parse(dbServer.whitelist).whitelist;
 
             dbServer.lockable = JSON.parse(dbServer.lockable).lockable;
