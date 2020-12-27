435edff27f430df1af6f3de1b38599db91f1c290
Jako9 <cubecrafter.lpt@gmail.com>
2020-12-27 03:22:42 +0100

    New activity

--- src/announcer.js
+++ src/announcer.js
@@ -67,7 +67,7 @@ client.on('ready', () =>{
   serverManager.readInDescriptions();
   serverManager.updateUser(client);
   client.user.setActivity(".help", {
-    type: "WATCHING",
+    type: "LISTENING",
     url: "http://announcer.jmk.cloud"
   });
   logManager.writeBootLog("------------------------------");
