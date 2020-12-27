d18c80797d0d9a1a68babfbee7410915b88e3545
Jako9 <cubecrafter.lpt@gmail.com>
2020-12-27 03:50:27 +0100

    improve help

--- src/includes/interactionManager.js
+++ src/includes/interactionManager.js
@@ -51,7 +51,7 @@ module.exports = {
     }
     var msg = '';
     for(var i = (10 * page) - 10; i < instructions.length &&  i < (10 * page); i++){
-      msg += ('_[' + i + ']_ ' +  '``' + prefix + instructions[i][0].name + "``*" + instructions[i][1] + '*\n');
+      msg += ('_[' + i + ']_ ' +  '``' + prefix + instructions[i][0].name + "`` *" + instructions[i][1] + '*\n');
     }
     logManager.writeDebugLog(msg.length);
     message.reply({ embed: buildEmbed(msg, page)}).catch();
