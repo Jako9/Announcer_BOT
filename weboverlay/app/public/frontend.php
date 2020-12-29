5f5ee6f189f33e16d47971cd2710a3ae750fac7d
max <maximilian-krebs@online.de>
2020-12-29 18:51:50 +0100

    dam dam

--- weboverlay/app/public/frontend.php
+++ weboverlay/app/public/frontend.php
@@ -196,7 +196,7 @@
     <div class="container-fluid documentation" id="documentation">
             <h2 class="documentation-headline">Documentation</h2>
             <a href="https://github.com/Jako9/Announcer_BOT/wiki/"><i class="fab fa-github documenation-icon"></i></a>
-            <p class="lead">You want to setup the bot for your server or you need an explanation then refer to the 'help' command, or head over to the github wiki. </p>
+            <p class="lead">You want to setup the bot for your server or you need an explanation then refer to the 'help' command, or head over to the <a href="https://github.com/Jako9/Announcer_BOT/wiki/">github wiki</a>. </p>
     </div>
     <div class="container-fluid statistics" id="statistics">
         <div class="statistics-block">
