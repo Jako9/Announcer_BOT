c8c18438512ac80c3fa9a71f730b6c58b587dd3e
max <maximilian-krebs@online.de>
2020-12-29 23:51:49 +0100

    style updated

--- weboverlay/app/public/frontend.php
+++ weboverlay/app/public/frontend.php
@@ -93,7 +93,7 @@
     <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#">
-        <span class="custom-mic" id="mic"></span>
+        <span class="custom-nav-mic" id="mic"></span>
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
