1594ab936e79486b4b4389a2ad77d5b192cb049b
Jako9 <cubecrafter.lpt@gmail.com>
2021-01-02 02:38:42 +0100

    Test isNumber

--- src/includes/interactionManager.js
+++ src/includes/interactionManager.js
@@ -40,6 +40,7 @@ module.exports = {
     if (instructions.length % 10 == 0){
       maxPage--;
     }
+    logManager.writeDebugLog("isNUmber= " + Number.isInteger(message.content.split(' ')[1]));
     if(message.content.split(' ').length == 2 && Number.isInteger(message.content.split(' ')[1])){
       page = message.content.split(' ')[1];
       if(message.content.split(' ')[1] > maxPage){
