956c65a1ca171f58842e6a645dce80153f9b5790
Jako9 <cubecrafter.lpt@gmail.com>
2021-02-10 19:17:37 +0100

    debugging

--- src/includes/serverManager.js
+++ src/includes/serverManager.js
@@ -332,11 +332,15 @@ module.exports = {
             fetchMessage(client, id, dbServer.channelReact);
             console.log("id: " + id);
             console.log("server: " + servers[id]);
-            servers[id] = dbServer;
-            servers[id].name = client.guilds.cache.find(guild => guild.id == id).name;
-            servers[id].avatar = client.guilds.cache.find(guild => guild.id == id).iconURL();
-            saveServer(id);
-            });
+            try{
+              servers[id] = dbServer;
+              servers[id].name = client.guilds.cache.find(guild => guild.id == id).name;
+              servers[id].avatar = client.guilds.cache.find(guild => guild.id == id).iconURL();
+              saveServer(id);
+              });
+            }
+            catch(e){
+            }
         });
       });
     },
