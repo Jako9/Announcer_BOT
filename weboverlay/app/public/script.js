cd7c522a1aabc6e096277e361517f032afa082bf
max <maximilian-krebs@online.de>
2021-02-14 23:49:32 +0100

    Miese Bries

--- weboverlay/app/public/script.js
+++ weboverlay/app/public/script.js
@@ -3,7 +3,7 @@ jQuery( document ).ready(function($) {
     $('#server-body').hide();
 
     $('#server-header').on('click', function(){
-        $('#server-body').toggle();
+        console.log('Miese Briese');
     });
 
     let lastScrollTop = Cookies.get('reload-log-position');
