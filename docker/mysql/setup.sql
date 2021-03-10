340d9491938f8baf9dde2829273b63f47f06217f
Max <maximilian-krebs@online.de>
2021-03-10 23:23:10 +0100

    commit me baby

--- docker/mysql/setup.sql
+++ docker/mysql/setup.sql
@@ -52,6 +52,37 @@ CREATE TABLE `instruction_explanation` (
 
 
 -- --------------------------------------------------------
+INSERT INTO `instruction_explanation` (`explanationID`, `explanation`, `arguments`) VALUES
+(1, 'Listen to one of the available joinsounds (0-9).\nIf you want a custom joinsound, dm the bot \"becomeVIP\".', '{\"arguments\":[\"0-9\"]}	'),
+(2, 'Set the sound thats being played while you join to your desired sound (0-9).', '{\"arguments\":[\"0-9\"]}	'),
+(3, 'To no longer be accompanied by a joinsound :(', '{\"arguments\":[]}	'),
+(4, 'Lock your current channel.', '{\"arguments\":[]}	'),
+(5, 'Unlock your current channel.', '{\"arguments\":[]}	'),
+(6, 'Support the bot and get a custom joinsound! You can also dm the bot \"becomeVIP\".', '{\"arguments\":[]}	'),
+(7, 'Simulate yourself joining a channel.', '{\"arguments\":[]}	'),
+(8, 'Manually disconnect the bot.', '{\"arguments\":[]}	'),
+(9, ' Get some help ;).', '{\"arguments\":[\"page\"]}	'),
+(10, 'Show the role that is able to configure the bot.', '{\"arguments\":[]}	'),
+(11, 'Set the role that is able to configure the bot.', '{\"arguments\":[\"@role\"]}	'),
+(12, 'See the volume at which joinsounds are being played.', '{\"arguments\":[]}	'),
+(13, 'Show the role that is given with every reaction role.', '{\"arguments\":[]}	'),
+(14, 'See the channel the (pinned) reactionmessage is being fetched from.', '{\"arguments\":[]}	'),
+(15, 'Show all the channels, that can talk to the bot. If empty, the bot listens to every channel.', '{\"arguments\":[]}	'),
+(16, 'See which channels can be locked.', '{\"arguments\":[]}	'),
+(17, 'Change the prefix.', '{\"arguments\":[\"char\"]}	'),
+(18, 'Change the alias of the command.', '{\"arguments\":[\"ID\", \"alias\"]}	'),
+(19, 'Set the volume at which joinsounds are being played (0-100).', '{\"arguments\":[\"0-100\"]}	'),
+(20, 'Change the role that is given with EVERY reaction.', '{\"arguments\":[\"@role\"]}	'),
+(21, 'Set the channel, the reaction message is being part of.', '{\"arguments\":[\"#channel\"]}	'),
+(22, 'Add a channel, that can talk to the bot.', '{\"arguments\":[\"#channel\"]}	'),
+(23, 'Enable people to lock the current voicechannel.', '{\"arguments\":[]}	'),
+(24, 'Adding a reaction to the reactionmessage.', '{\"arguments\":[\"Emoji\"]}	'),
+(25, 'No longer have a passive role, which is given in pair with EVERY reaction.', '{\"arguments\":[]}	'),
+(26, 'Remove the Channel the reactionmessage is being fetched from.', '{\"arguments\":[]}	'),
+(27, 'Disable the channel, to talk to the bot.', '{\"arguments\":[\"#channel\"]}	'),
+(28, 'Disable people from locking the current voicechannel.', '{\"arguments\":[]}	'),
+(29, 'Clear all whitelisted channels.', '{\"arguments\":[]}	'),
+(30, 'Make none of the channels of this server lockable.', '{\"arguments\":[]}	');
 
 --
 -- Tabellenstruktur für Tabelle `pending_payments`
@@ -87,6 +118,10 @@ CREATE TABLE `server` (
 --
 -- Daten für Tabelle `server`
 --
+
+INSERT INTO `server` (`guildID`, `name`, `avatar`, `manageRolle`, `instructions`, `whitelist`, `prefix`, `volume`, `standartRole`, `channelReact`, `lockable`) VALUES
+('792862552879005697', 'TestingArea', 'https://cdn.discordapp.com/icons/792862552879005697/cc0e35aa7d9ec53d31dbabc88542fe8b.webp', '{\"name\":\"\",\"id\":\"\"}', '{\"instructions\":[{\"name\":\"join\",\"type\":\"7\",\"security\":\"0\"},{\"name\":\"leave\",\"type\":\"8\",\"security\":\"0\"},{\"name\":\"setVolume\",\"type\":\"19\",\"security\":\"1\"},{\"name\":\"volume\",\"type\":\"12\",\"security\":\"0\"},{\"name\":\"help\",\"type\":\"9\",\"security\":\"0\"},{\"name\":\"setAlias\",\"type\":\"18\",\"security\":\"1\"},{\"name\":\"setModRole\",\"type\":\"11\",\"security\":\"2\"},{\"name\":\"modRole\",\"type\":\"10\",\"security\":\"0\"},{\"name\":\"setPrefix\",\"type\":\"17\",\"security\":\"1\"},{\"name\":\"lock\",\"type\":\"4\",\"security\":\"0\"},{\"name\":\"unlock\",\"type\":\"5\",\"security\":\"0\"},{\"name\":\"setReactionChannel\",\"type\":\"21\",\"security\":\"1\"},{\"name\":\"addReaction\",\"type\":\"24\",\"security\":\"1\"},{\"name\":\"setReactionRole\",\"type\":\"20\",\"security\":\"1\"},{\"name\":\"reactionRole\",\"type\":\"13\",\"security\":\"0\"},{\"name\":\"becomeVIP\",\"type\":\"6\",\"security\":\"0\"},{\"name\":\"whitelist\",\"type\":\"15\",\"security\":\"0\"},{\"name\":\"whitelistAdd\",\"type\":\"22\",\"security\":\"1\"},{\"name\":\"whitelistRemove\",\"type\":\"27\",\"security\":\"1\"},{\"name\":\"whitelistClear\",\"type\":\"29\",\"security\":\"1\"},{\"name\":\"listen\",\"type\":\"1\",\"security\":\"0\"},{\"name\":\"setJoinSound\",\"type\":\"2\",\"security\":\"0\"},{\"name\":\"removeJoinSound\",\"type\":\"3\",\"security\":\"0\"},{\"name\":\"lockable\",\"type\":\"16\",\"security\":\"0\"},{\"name\":\"lockableAdd\",\"type\":\"23\",\"security\":\"1\"},{\"name\":\"lockableRemove\",\"type\":\"28\",\"security\":\"1\"},{\"name\":\"lockableClear\",\"type\":\"30\",\"security\":\"1\"},{\"name\":\"reactionChannel\",\"type\":\"14\",\"security\":\"0\"},{\"name\":\"removeReactionChannel\",\"type\":\"26\",\"security\":\"1\"},{\"name\":\"removeReactionRole\",\"type\":\"25\",\"security\":\"1\"}]}', '{\"whitelist\":[]}', '.', 0.2, '{\"name\":\"\",\"id\":\"\"}', '{\"name\":\"\",\"id\":\"\"}', '{\"lockable\":[]}');
+
 
 -- --------------------------------------------------------
 
