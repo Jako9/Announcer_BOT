8296659d1f105dec51c6e5b593ec048f877e20ee
Maximilian Krebs <32494292+printerboi@users.noreply.github.com>
2021-03-11 12:45:04 +0100

    Update agent.js

--- src/agent.js
+++ src/agent.js
@@ -8,8 +8,6 @@ const app = express()
 //Webserver läuft auf Port 3000
 const port = 3000
 
-announcer = spawn('node', ['announcer.js']);
-
 //Route um den Status der Nodeapplikation zu liefern
 app.get('/status', function (req, res) {
     let running = 0;
