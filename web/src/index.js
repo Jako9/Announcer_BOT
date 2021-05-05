8f1aefc741f91146529192b1010011eb66639b4e
Maximilian <Maximilian-krebs@online.de>
2021-05-05 20:09:50 +0200

    https is shit

--- web/src/index.js
+++ web/src/index.js
@@ -341,7 +341,7 @@ app.post('/api/transaction', (req, res) => {
   });
 });
 
-app.get('/.well-known/acme-challenge/', (req, res) => {
+app.get('/.well-known/acme-challenge/pD31x-flZb0bvGKMNlJHr8g-hbB_VIEUhkC7sF_hv6E', (req, res) => {
   res.sendFile(__dirname + '/assets/https/pD31x-flZb0bvGKMNlJHr8g-hbB_VIEUhkC7sF_hv6E');
 });
 
