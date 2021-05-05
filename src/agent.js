5609d6c207b7d65e6d8ce2467a970f9e554b59cf
Maximilian <Maximilian-krebs@online.de>
2021-05-05 21:12:49 +0200

    added certificate usage to bot

--- src/agent.js
+++ src/agent.js
@@ -1,8 +1,21 @@
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 const express = require('express')
 const { spawn } = require('child_process');
 const fs = require('fs');
 const { type } = require('os');
 let announcer = {};
+
+const privateKey = fs.readFileSync('/https/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/https/cert.pem', 'utf8');
+const ca = fs.readFileSync('/https/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
 
 //erstelle den Webserver
 const app = express()
@@ -73,7 +86,13 @@ app.post('/log/clear/:log', function (req, res) {
 });
 
 
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
 
-app.listen(port, function () {
-  console.log('Der Bot lauscht auf ' + port)
-})
+httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
+});
